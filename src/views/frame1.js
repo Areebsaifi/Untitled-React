8019ebc81f57902f999c7dfeefa886e9dadfbeab
Areebsaifi <areeb.saifi002@gmail.com>
2024-05-22 17:21:24 +0530

    Commit from TeleportHQ

---
+++ src/views/frame1.js
@@ -0,0 +1,218 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './frame1.css'
+
+const Frame1 = (props) => {
+  return (
+    <div className="frame1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="frame1-frame1">
+        <div className="frame1-landingpage01">
+          <div className="frame1-frame1321316142">
+            <div className="frame1-copy">
+              <span className="frame1-text Title">
+                <span>Landing page title</span>
+              </span>
+              <span className="frame1-text02 Subheading">
+                <span>And a subheading describing your site, too</span>
+              </span>
+              <button className="frame1-button">
+                <span className="frame1-text04 Smalltext">
+                  <span>Button</span>
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className="frame1-image-dragintofill"></div>
+          <div className="frame1-heading">
+            <span className="frame1-text06 Heading">
+              <span>Heading</span>
+            </span>
+            <span className="frame1-text08 Subheading">
+              <span>Subheading to introduce testimonials</span>
+            </span>
+          </div>
+          <div className="frame1-cards">
+            <div className="frame1-customer-quote">
+              <span className="frame1-text10 Bodytext">
+                <span>“A terrific piece of praise”</span>
+              </span>
+              <div className="frame1-avatar">
+                <img
+                  alt="Avatar1190"
+                  src="https://play.teleporthq.io/static/svg/default-img.svg"
+                  className="frame1-avatar1"
+                />
+                <div className="frame1-frame2610301">
+                  <span className="frame1-text12 Smalltext">
+                    <span>Name</span>
+                  </span>
+                  <span className="frame1-text14 Smalltext">
+                    <span>Description</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="frame1-customer-quote1">
+              <span className="frame1-text16 Bodytext">
+                <span>“A fantastic bit of feedback”</span>
+              </span>
+              <div className="frame1-avatar2">
+                <img
+                  alt="Avatar1197"
+                  src="https://play.teleporthq.io/static/svg/default-img.svg"
+                  className="frame1-avatar3"
+                />
+                <div className="frame1-frame26103011">
+                  <span className="frame1-text18 Smalltext">
+                    <span>Name</span>
+                  </span>
+                  <span className="frame1-text20 Smalltext">
+                    <span>Description</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="frame1-customer-quote2">
+              <span className="frame1-text22 Bodytext">
+                <span>“A genuinely glowing review”</span>
+              </span>
+              <div className="frame1-avatar4">
+                <img
+                  alt="Avatar1204"
+                  src="https://play.teleporthq.io/static/svg/default-img.svg"
+                  className="frame1-avatar5"
+                />
+                <div className="frame1-frame26103012">
+                  <span className="frame1-text24 Smalltext">
+                    <span>Name</span>
+                  </span>
+                  <span className="frame1-text26 Smalltext">
+                    <span>Description</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame1-navigation">
+            <div className="frame1-items">
+              <span className="frame1-text28 Bodytext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text30 Bodytext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text32 Bodytext">
+                <span>Page</span>
+              </span>
+              <button className="frame1-button1">
+                <span className="frame1-text34 Smalltext">
+                  <span>Button</span>
+                </span>
+              </button>
+            </div>
+            <span className="frame1-text36 Bodytext">
+              <span>RAIZA</span>
+            </span>
+          </div>
+          <div className="frame1-navigation-footer">
+            <div className="frame1-items1">
+              <span className="frame1-text38 Smalltext">
+                <span>Topic</span>
+              </span>
+              <span className="frame1-text40 Smalltext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text42 Smalltext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text44 Smalltext">
+                <span>Page</span>
+              </span>
+            </div>
+            <div className="frame1-items2">
+              <span className="frame1-text46 Smalltext">
+                <span>Topic</span>
+              </span>
+              <span className="frame1-text48 Smalltext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text50 Smalltext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text52 Smalltext">
+                <span>Page</span>
+              </span>
+            </div>
+            <div className="frame1-items3">
+              <span className="frame1-text54 Smalltext">
+                <span>Topic</span>
+              </span>
+              <span className="frame1-text56 Smalltext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text58 Smalltext">
+                <span>Page</span>
+              </span>
+              <span className="frame1-text60 Smalltext">
+                <span>Page</span>
+              </span>
+            </div>
+            <span className="frame1-text62 Subheading">
+              <span>Site name</span>
+            </span>
+            <div className="frame1-social-icons">
+              <button className="frame1-buttons-icon">
+                <div className="frame1-icon">
+                  <img
+                    alt="Icon1236"
+                    src="https://play.teleporthq.io/static/svg/default-img.svg"
+                    className="frame1-icon1"
+                  />
+                </div>
+              </button>
+              <button className="frame1-buttons-icon1">
+                <div className="frame1-icon2">
+                  <img
+                    alt="Icon1241"
+                    src="https://play.teleporthq.io/static/svg/default-img.svg"
+                    className="frame1-icon3"
+                  />
+                </div>
+              </button>
+              <button className="frame1-buttons-icon2">
+                <div className="frame1-icon4">
+                  <img
+                    alt="Icon1246"
+                    src="https://play.teleporthq.io/static/svg/default-img.svg"
+                    className="frame1-icon5"
+                  />
+                </div>
+              </button>
+              <button className="frame1-buttons-icon3">
+                <div className="frame1-icon6">
+                  <img
+                    alt="Icon1251"
+                    src="https://play.teleporthq.io/static/svg/default-img.svg"
+                    className="frame1-icon7"
+                  />
+                </div>
+              </button>
+            </div>
+            <img
+              alt="Divider1254"
+              src="https://play.teleporthq.io/static/svg/default-img.svg"
+              className="frame1-divider"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Frame1
